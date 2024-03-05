b108bedf3dd434906b40aa951923e999466188d2
João Vcitor Coelho <jvcoelho08@outlook.com>
2024-03-05 17:26:56 -0300

    Category

---
+++ src/main/java/com/educandoeweb/course/services/CategoryService.java
@@ -0,0 +1,24 @@
+package com.educandoeweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.educandoeweb.course.entities.Category;
+import com.educandoeweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService  {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id){
+		Optional<Category> obj =  repository.findById(id);
+		return obj.get();
+	}
+}
